45e459a1f93d74f261acf981ab9c7d6c052f5621
dennistonc <dennistonc@gmail.com>
2020-06-09 22:08:05 -0500

    Updates

---
+++ precache-manifest.9b9d33d5a17a076a8c73706bc1c40609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de588db5ffad9a51a53c1c50c5dc5c7e",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "2eec23f2fd630f35f8c7",
+    "url": "/clicky-game/static/css/main.50cd7c63.chunk.css"
+  },
+  {
+    "revision": "55ea0a1c41e3cf21f12c",
+    "url": "/clicky-game/static/js/2.378b37ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.378b37ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eec23f2fd630f35f8c7",
+    "url": "/clicky-game/static/js/main.d8c1f088.chunk.js"
+  },
+  {
+    "revision": "ddea091f236f02df6e9c",
+    "url": "/clicky-game/static/js/runtime-main.de0ba425.js"
+  }
+]);
